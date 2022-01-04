bfeb6fabafec2b4da354934638547bf2652d9fc4
Gökhan Üstüner <gokhanustuner@gmail.com>
2022-01-04 23:05:58 +0300

    Template improvements for register and login pages

--- members/urls.py
+++ members/urls.py
@@ -5,6 +5,6 @@ from . import views
 urlpatterns = [
     path('<int:pk>/', views.show, name='members.show'),
     path('home/', views.home, name='members.home'),
-    path('register/', views.Register.as_view(), name='members.register'),
+    path('register/', views.RegisterView.as_view(), name='members.register'),
     path('login/', views.LoginView.as_view(), name='members.login'),
 ]
